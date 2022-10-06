460130dd25aa6d37478f089985398aa972a1bd35
guilhermecostam <guicm007@gmail.com>
2022-10-06 10:35:02 -0300

    feat: resolvendo questões do json5

--- promises/script.js
+++ promises/script.js
@@ -1,21 +1,35 @@
-let cervejas = []
+let cervejas = [];
 
 //cs é um array de cervejas
-const carregarDiv = cs => {
-    const div = document.getElementById("cervejasDiv")
-    const itensHtml = cs.map( ({name,alcohol}) => `<div>${name} -- ${alcohol}</div>` )
-    div.innerHTML = `${itensHtml.join("\n")}`
+const carregarDiv = (cs, elementId, cabecalhoItems, obj) => {
+    const div = document.getElementById(elementId);
+    const cabecalho = cabecalhoItems.map(item => `<th> ${item}</th>` ).join("");
+    const itensHtml = cs.map( item => `<tr>`+ obj.map(
+        objet=> `<td>${item[objet]}</td>`
+    ).join("")+`</tr>` );
+
+    div.innerHTML = `
+        <table class="table table-striped">
+            <tr>${cabecalho}</tr> ${itensHtml.join("\n")}
+        </table>
+    `;
 }
 
-async function carregarCervejas(){
+async function carregarCervejas(
+    quantidade=3,
+    link="https://random-data-api.com/api/v2/beers?size=",
+    cabecalhoItems=["Nome", "Alcool", "Amargor", "Estilo"],
+    obj=["name", "alcohol", "ibu", "style"],
+    id="cervejasDiv"
+){
     try {
-        let res = await fetch("https://random-data-api.com/api/v2/beers?size=3")
-        cervejas = await res.json()
-        carregarDiv(cervejas)
+        let res = await fetch(link+quantidade);
+        cervejas = await res.json();
+        carregarDiv(cervejas, id, cabecalhoItems, obj);
     } catch(err) {
-        document.getElementById("cervejasDiv").innerHTML = "Fudeu..."
+        document.getElementById("cervejasDiv").innerHTML = "Fudeu...";
     }
 }
 
-let botao = document.getElementById("botaoCarregar")
-botao.addEventListener("click", () => carregarCervejas() )
+let botao = document.getElementById("botaoCarregar");
+botao.addEventListener("click", () => carregarCervejas() );
