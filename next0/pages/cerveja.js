fe68faf6196295957788104d57fe20128813300b
guilhermecostam <guicm007@gmail.com>
2022-10-21 15:47:47 -0300

    feat: adicionando receita next0

---
+++ next0/pages/cerveja.js
@@ -0,0 +1,24 @@
+function Cerveja(){
+    return (
+        <table>
+            <tr>
+                <th>Nome</th>
+                <th>Avaliação</th>
+            </tr>
+            <tr>
+                <td>Kaiser</td>
+                <td>4.3</td>
+            </tr>
+            <tr>
+                <td>Devassa</td>
+                <td>7.9</td>
+            </tr>
+            <tr>
+                <td>Brahma Chopp</td>
+                <td>7.3</td>
+            </tr>
+        </table>
+    )
+}
+
+export default Cerveja
---
+++ next0/pages/index.js
@@ -0,0 +1,10 @@
+import Cerveja from './cerveja.js'
+
+export default function Home(){
+    return (
+        <>
+            <h2>Cervejas:</h2>
+            <Cerveja/>
+        </>
+    )
+}
